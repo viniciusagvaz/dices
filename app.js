751358679200e53db20163d491559966dec6aeac
Vinícius Vaz <agvazvinicius@gmail.com>
2024-08-01 15:04:53 -0300

    final refact

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let dice1;
-let dice2;
+let dice1 = 1;
+let dice2 = 1;
 const button = document.querySelector("button");
 
-const dice = {
+const Dice = {
   sides: 6,
   roll: function () {
     const diceResult = Math.floor(Math.random() * this.sides) + 1;
@@ -11,41 +11,46 @@ const dice = {
 };
 
 const rollDice = () => {
-  dice1 = dice.roll();
-  dice2 = dice.roll();
+  dice1 = Dice.roll();
+  dice2 = Dice.roll();
 
   renderDices();
   renderResults();
-};
-
-const rollAgain = () => {
-  rollDice();
 };
 
 const renderDices = () => {
   const diceImg1 = document.querySelector(".img1");
   const diceImg2 = document.querySelector(".img2");
 
-  diceImg1.src = `./images/dice${dice1}.png`;
-  diceImg2.src = `./images/dice${dice2}.png`;
+  if (diceImg1 && diceImg2) {
+    diceImg1.src = `./images/dice${dice1}.png`;
+    diceImg2.src = `./images/dice${dice2}.png`;
+  } else {
+    console.error("Dice images not found");
+  }
 };
 
 const renderResults = () => {
   let result = document.querySelector(".result");
 
-  if (dice1 > dice2) {
-    return (result.textContent = `Player 1 wins!`);
+  if (result){
+    if (dice1 > dice2) {
+      return (result.textContent = `Player 1 wins!`);
+    }
+  
+    if (dice1 < dice2) {
+      return (result.textContent = `Player 2 wins!`);
+    }
+  
+    if (dice1 === dice2) {
+      return (result.textContent = `It's a draw!`);
+    }
+  } else {
+    console.error(`Could not found the results element`)
   }
 
-  if (dice1 < dice2) {
-    return (result.textContent = `Player 2 wins!`);
-  }
-
-  if (dice1 === dice2) {
-    return (result.textContent = `It's a draw!`);
-  }
 };
 
-button.addEventListener("click", rollAgain);
+button.addEventListener("click", rollDice);
 
 rollDice();
